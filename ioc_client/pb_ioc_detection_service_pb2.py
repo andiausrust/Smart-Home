588c85e02ab5293f8e0562e682766535647b9581
Stefan Schwandter <s.schwandter@cybertrap.com>
2018-10-16 13:30:58 +0200

    Regenerated protobuf (async->async_watcher)

--- ioc_client/pb_ioc_detection_service_pb2.py
+++ ioc_client/pb_ioc_detection_service_pb2.py
@@ -12,8 +12,8 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-import ioc_client.pb_dal_pb2 as pb__dal__pb2
-import ioc_client.pb_events_pb2 as pb__events__pb2
+import pb_dal_pb2 as pb__dal__pb2
+import pb_events_pb2 as pb__events__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
@@ -21,7 +21,7 @@ DESCRIPTOR = _descriptor.FileDescriptor(
   package='com.cybertrap.protobuf.iocdetectionservice',
   syntax='proto3',
   serialized_options=_b('Z?git.vie.cybertrap.com/generated/protobuf-go/iocdetectionservice'),
-  serialized_pb=_b('\n\x1epb_ioc_detection_service.proto\x12*com.cybertrap.protobuf.iocdetectionservice\x1a\x0cpb_dal.proto\x1a\x0fpb_events.proto\"O\n\x14GetLiveAlertsRequest\x12\x0c\n\x04\x66rom\x18\x01 \x01(\x04\x12\n\n\x02to\x18\x02 \x01(\x04\x12\x0e\n\x06offset\x18\x03 \x01(\r\x12\r\n\x05\x63ount\x18\x04 \x01(\r\"\xd7\x01\n\x15GetLiveAlertsResponse\x12W\n\x06\x61lerts\x18\x01 \x03(\x0b\x32G.com.cybertrap.protobuf.iocdetectionservice.GetLiveAlertsResponse.Alert\x12\r\n\x05total\x18\x02 \x01(\x04\x1aV\n\x05\x41lert\x12\x0b\n\x03ioc\x18\x01 \x01(\x0c\x12\x0b\n\x03ips\x18\x02 \x03(\t\x12\x33\n\x05\x65vent\x18\x03 \x01(\x0b\x32$.com.cybertrap.protobuf.events.Event\"Q\n\x17SetFalsePositiveRequest\x12\x15\n\rfalsePositive\x18\x01 \x01(\x08\x12\x10\n\x08\x65ventIds\x18\x02 \x03(\x04\x12\r\n\x05\x61sync\x18\x03 \x01(\x08\"\x1a\n\x18SetFalsePositiveResponse\"\x93\x01\n\x13SetIOCStatusRequest\x12J\n\nioc_status\x18\x01 \x01(\x0e\x32\x36.com.cybertrap.protobuf.events.BaseEvent.IOCStatusEnum\x12\x0f\n\x07\x63omment\x18\x02 \x01(\t\x12\x10\n\x08\x65ventIds\x18\x03 \x03(\x03\x12\r\n\x05\x61sync\x18\x04 \x01(\x08\"\x16\n\x14SetIOCStatusResponse\"\x8d\x01\n\x13\x46\x61lsePositiveChange\x12:\n\x08oldEvent\x18\x01 \x01(\x0b\x32(.com.cybertrap.protobuf.dal.EventMessage\x12:\n\x08newEvent\x18\x02 \x01(\x0b\x32(.com.cybertrap.protobuf.dal.EventMessage\"s\n\x19\x46\x61lsePositiveChangeNotice\x12V\n\rchangedEvents\x18\x01 \x03(\x0b\x32?.com.cybertrap.protobuf.iocdetectionservice.FalsePositiveChange\"^\n\x0f\x41lertAttachment\x12K\n\x07matches\x18\x01 \x03(\x0b\x32:.com.cybertrap.protobuf.iocdetectionservice.AlertRuleMatch\"\x95\x01\n\x0e\x41lertRuleMatch\x12\x11\n\truleGroup\x18\x01 \x01(\t\x12\x19\n\x11serviceInstanceId\x18\x02 \x01(\t\x12U\n\x10suspiciousEvents\x18\x03 \x03(\x0b\x32;.com.cybertrap.protobuf.iocdetectionservice.SuspiciousEvent\"4\n\x0fSuspiciousEvent\x12\x0f\n\x07\x65ventId\x18\x01 \x01(\x03\x12\x10\n\x08sourceIp\x18\x02 \x01(\t\"m\n\x0eMarkIOCRequest\x12L\n\x06\x61lerts\x18\x01 \x03(\x0b\x32<.com.cybertrap.protobuf.iocdetectionservice.SmartinizerAlert\x12\r\n\x05\x61sync\x18\x03 \x01(\x08\"\x11\n\x0fMarkIOCResponse\"\x9a\x01\n\x10SmartinizerAlert\x12\x16\n\x0eprocessEventId\x18\x01 \x01(\x03\x12\x0f\n\x07\x63omment\x18\x02 \x01(\t\x12]\n\x0e\x66ileEventAlert\x18\x03 \x03(\x0b\x32\x45.com.cybertrap.protobuf.iocdetectionservice.SmartinizerFileEventAlert\"B\n\x19SmartinizerFileEventAlert\x12\x14\n\x0c\x66ileEventIds\x18\x01 \x01(\x03\x12\x0f\n\x07\x63omment\x18\x02 \x01(\tBAZ?git.vie.cybertrap.com/generated/protobuf-go/iocdetectionserviceb\x06proto3')
+  serialized_pb=_b('\n\x1epb_ioc_detection_service.proto\x12*com.cybertrap.protobuf.iocdetectionservice\x1a\x0cpb_dal.proto\x1a\x0fpb_events.proto\"O\n\x14GetLiveAlertsRequest\x12\x0c\n\x04\x66rom\x18\x01 \x01(\x04\x12\n\n\x02to\x18\x02 \x01(\x04\x12\x0e\n\x06offset\x18\x03 \x01(\r\x12\r\n\x05\x63ount\x18\x04 \x01(\r\"\xd7\x01\n\x15GetLiveAlertsResponse\x12W\n\x06\x61lerts\x18\x01 \x03(\x0b\x32G.com.cybertrap.protobuf.iocdetectionservice.GetLiveAlertsResponse.Alert\x12\r\n\x05total\x18\x02 \x01(\x04\x1aV\n\x05\x41lert\x12\x0b\n\x03ioc\x18\x01 \x01(\x0c\x12\x0b\n\x03ips\x18\x02 \x03(\t\x12\x33\n\x05\x65vent\x18\x03 \x01(\x0b\x32$.com.cybertrap.protobuf.events.Event\"Q\n\x17SetFalsePositiveRequest\x12\x15\n\rfalsePositive\x18\x01 \x01(\x08\x12\x10\n\x08\x65ventIds\x18\x02 \x03(\x04\x12\r\n\x05\x61sync\x18\x03 \x01(\x08\"\x1a\n\x18SetFalsePositiveResponse\"\x93\x01\n\x13SetIOCStatusRequest\x12J\n\nioc_status\x18\x01 \x01(\x0e\x32\x36.com.cybertrap.protobuf.events.BaseEvent.IOCStatusEnum\x12\x0f\n\x07\x63omment\x18\x02 \x01(\t\x12\x10\n\x08\x65ventIds\x18\x03 \x03(\x03\x12\r\n\x05\x61sync\x18\x04 \x01(\x08\"\x16\n\x14SetIOCStatusResponse\"\x8d\x01\n\x13\x46\x61lsePositiveChange\x12:\n\x08oldEvent\x18\x01 \x01(\x0b\x32(.com.cybertrap.protobuf.dal.EventMessage\x12:\n\x08newEvent\x18\x02 \x01(\x0b\x32(.com.cybertrap.protobuf.dal.EventMessage\"s\n\x19\x46\x61lsePositiveChangeNotice\x12V\n\rchangedEvents\x18\x01 \x03(\x0b\x32?.com.cybertrap.protobuf.iocdetectionservice.FalsePositiveChange\"^\n\x0f\x41lertAttachment\x12K\n\x07matches\x18\x01 \x03(\x0b\x32:.com.cybertrap.protobuf.iocdetectionservice.AlertRuleMatch\"\x95\x01\n\x0e\x41lertRuleMatch\x12\x11\n\truleGroup\x18\x01 \x01(\t\x12\x19\n\x11serviceInstanceId\x18\x02 \x01(\t\x12U\n\x10suspiciousEvents\x18\x03 \x03(\x0b\x32;.com.cybertrap.protobuf.iocdetectionservice.SuspiciousEvent\"4\n\x0fSuspiciousEvent\x12\x0f\n\x07\x65ventId\x18\x01 \x01(\x03\x12\x10\n\x08sourceIp\x18\x02 \x01(\t\"u\n\x0eMarkIOCRequest\x12L\n\x06\x61lerts\x18\x01 \x03(\x0b\x32<.com.cybertrap.protobuf.iocdetectionservice.SmartinizerAlert\x12\x15\n\rasync_watcher\x18\x03 \x01(\x08\"\x11\n\x0fMarkIOCResponse\"\x9a\x01\n\x10SmartinizerAlert\x12\x16\n\x0eprocessEventId\x18\x01 \x01(\x03\x12\x0f\n\x07\x63omment\x18\x02 \x01(\t\x12]\n\x0e\x66ileEventAlert\x18\x03 \x03(\x0b\x32\x45.com.cybertrap.protobuf.iocdetectionservice.SmartinizerFileEventAlert\"B\n\x19SmartinizerFileEventAlert\x12\x14\n\x0c\x66ileEventIds\x18\x01 \x01(\x03\x12\x0f\n\x07\x63omment\x18\x02 \x01(\tBAZ?git.vie.cybertrap.com/generated/protobuf-go/iocdetectionserviceb\x06proto3')
   ,
   dependencies=[pb__dal__pb2.DESCRIPTOR,pb__events__pb2.DESCRIPTOR,])
 
@@ -505,7 +505,7 @@ _MARKIOCREQUEST = _descriptor.Descriptor(
       is_extension=False, extension_scope=None,
       serialized_options=None, file=DESCRIPTOR),
     _descriptor.FieldDescriptor(
-      name='async', full_name='com.cybertrap.protobuf.iocdetectionservice.MarkIOCRequest.async', index=1,
+      name='async_watcher', full_name='com.cybertrap.protobuf.iocdetectionservice.MarkIOCRequest.async_watcher', index=1,
       number=3, type=8, cpp_type=7, label=1,
       has_default_value=False, default_value=False,
       message_type=None, enum_type=None, containing_type=None,
@@ -524,7 +524,7 @@ _MARKIOCREQUEST = _descriptor.Descriptor(
   oneofs=[
   ],
   serialized_start=1256,
-  serialized_end=1365,
+  serialized_end=1373,
 )
 
 
@@ -547,8 +547,8 @@ _MARKIOCRESPONSE = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1367,
-  serialized_end=1384,
+  serialized_start=1375,
+  serialized_end=1392,
 )
 
 
@@ -592,8 +592,8 @@ _SMARTINIZERALERT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1387,
-  serialized_end=1541,
+  serialized_start=1395,
+  serialized_end=1549,
 )
 
 
@@ -630,8 +630,8 @@ _SMARTINIZERFILEEVENTALERT = _descriptor.Descriptor(
   extension_ranges=[],
   oneofs=[
   ],
-  serialized_start=1543,
-  serialized_end=1609,
+  serialized_start=1551,
+  serialized_end=1617,
 )
 
 _GETLIVEALERTSRESPONSE_ALERT.fields_by_name['event'].message_type = pb__events__pb2._EVENT
